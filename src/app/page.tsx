11d75b76a4d6f41ef063c108fd449a864f82f0a9
hayagerwin <74968696+hayagerwin@users.noreply.github.com>
2024-03-26 22:14:11 +0800

    Add Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* Title  */}
-          <motion.h1 variants={fadeIn("down", 0.2)} initial="hidden" animate="show" exit="hidden" className="h1">
+          <motion.h1 variants={fadeIn("down", 0.2)} initial="hidden" animate="show" exit="hidden" className="h1 z-30">
             Transforming Ideas <br /> Into
             <span className="text-accent"> Digital Reality</span>
           </motion.h1>
@@ -21,7 +21,7 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 z-30">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati similique optio possimus incidunt praesentium est esse, exercitationem vitae
             aliquid reprehenderit?
           </motion.p>
