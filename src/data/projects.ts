2506fdc74360f865256b026fe203c69dbc36872d
hayagerwin <74968696+hayagerwin@users.noreply.github.com>
2024-04-18 08:37:32 +0800

    Update Info for Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,9 +18,9 @@ export const projectData: Project[] = [
     category: "Reservation",
     year: 2021,
     team: ["Erwin Hayag"],
-    client: ["Georgia Lynn M. Villamor"],
+    client: ["Dr. Georgia Lynn M. Villamor"],
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas veniam tempore, soluta minima vel sapiente suscipit culpa eligendi in modi! Molestiae velit impedit unde nam.",
+      "Lynn Villamor Dental Clinic is a state-of-the-art reservation website tailored for patients seeking professional dental care. The platform simplifies the appointment-making process, allowing users to book visits with ease and confidence. Behind the scenes, the clinic’s administration benefits from a robust management system capable of handling patient records, scheduling, and other essential tasks. This digital solution empowers the clinic to offer an enhanced patient experience while maintaining high standards of care and organization.",
     slides: [
       {
         page: "Home Page",
@@ -45,11 +45,11 @@ export const projectData: Project[] = [
     title: "Lucky Star Grocery Store",
     cover: "/assets/images/lucky-star/cover.png",
     category: "E-commerce",
-    year: 2021,
+    year: 2022,
     team: ["Erwin Hayag"],
     client: ["Nenejj Store"],
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas veniam tempore, soluta minima vel sapiente suscipit culpa eligendi in modi! Molestiae velit impedit unde nam.",
+      "Lucky Star Grocery is an innovative e-commerce platform designed to revolutionize the local grocery shopping experience. With a commitment to convenience and speed, Lucky Star Grocery offers a seamless online shopping solution, ensuring that customers in nearby areas can enjoy same-day delivery of their favorite products. The website boasts a user-friendly interface, real-time inventory updates, and secure payment options, making it a reliable and efficient choice for those who value their time and comfort.",
     slides: [
       {
         page: "Home Page",
