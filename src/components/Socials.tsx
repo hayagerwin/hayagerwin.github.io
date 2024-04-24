6eda0cbc0452bffa600005e3edafea3775e6e6d4
hayagerwin <74968696+hayagerwin@users.noreply.github.com>
2024-04-24 20:59:45 +0800

    Minor Adjustment

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { RiGithubLine, RiTwitterXLine } from "react-icons/ri";
+import { RiGithubLine, RiLinkedinLine, RiTwitterXLine } from "react-icons/ri";
 
 const Socials = () => {
   return (
@@ -10,6 +10,9 @@ const Socials = () => {
       <Link href={"https://twitter.com/reedro85"} className="hover:text-accent transition-all duration-300" target="_blank">
         <RiTwitterXLine />
       </Link>
+      {/* <Link href={"https://www.linkedin.com/in/hayagerwin"} className="hover:text-accent transition-all duration-300" target="_blank">
+        <RiLinkedinLine />
+      </Link> */}
     </div>
   );
 };
